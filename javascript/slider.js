7fbf5082736351e64767cb61289af906a642f323
Shobhith-BGS-IT <shobhithsbgspsit@gmail.com>
2022-06-25 21:00:11 +0530

    deleated

--- javascript/slider.js
+++
@@ -1,8 +0,0 @@
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio1' + counter).checked = true;
-    counter++;
-    if(counter > 5){
-        counter = 1;
-    }
-}, 5000);
