5505dadec7e862a244f498f94d804a2cea3e500e
Shobhith-BGS-IT <shobhithsbgspsit@gmail.com>
2022-07-08 20:24:58 +0530

    added slider

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,8 +1,80 @@
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio1' + counter).checked = true;
-    counter++;
-    if(counter > 5){
-        counter = 1;
+const slider =document.querySelector(".slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+// image slider next button
+nextBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides - 1)){
+        slideNumber = 0;
     }
-}, 5000);
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
+
+//image slider previous button
+prevBtn.addEventListener("click", () => {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+    });
+
+    slideNumber--;
+
+    if(slideNumber < 0){
+        slideNumber = numberOfSlides - 1;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+});
+
+//image slider autoplay
+var playSlider;
+
+var repeater = () => {
+    playSlider = setInterval(function(){
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+        slideIcons.forEach((slideIcon) => {
+            slideIcon.classList.remove("active");
+        });
+    
+        slideNumber++;
+    
+        if(slideNumber > (numberOfSlides - 1)){
+            slideNumber = 0;
+        }
+    
+        slides[slideNumber].classList.add("active");
+        slideIcons[slideNumber].classList.add("active");
+    }, 3000);
+}
+repeater();
+
+//stop the image slider autoplay on mouseover
+slider.addEventListener("mouseover", () => {
+    clearInterval(playSlider);
+});
+
+//start the image slider autoplay on mouseout
+slider.addEventListener("mouseout", () => {
+    repeater();
+});
+
